bc232b9d0017fc59d9ff4e1c4c07ffdd42446d9b
Elijah Santos <65294557+jah-peg@users.noreply.github.com>
2021-04-21 18:38:10 +0800

    added intro page, login page with school logo
    Signed-off-by: Elijah Santos <65294557+jah-peg@users.noreply.github.com>

---
+++ app/src/main/java/com/example/rtu_student_portal/intro_page.java
@@ -0,0 +1,16 @@
+package com.example.rtu_student_portal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class intro_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro_page);
+    }
+
+
+}
