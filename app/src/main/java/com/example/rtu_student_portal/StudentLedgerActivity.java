eff10080d6827e90b52e148706a9e7f0ccb068a8
John Leonard Sabangan <leonardsabangan@gmail.com>
2021-06-11 14:28:26 +0800

    Kulang pa ng konting textview

---
+++ app/src/main/java/com/example/rtu_student_portal/StudentLedgerActivity.java
@@ -0,0 +1,14 @@
+package com.example.rtu_student_portal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StudentLedgerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_ledger);
+    }
+}
